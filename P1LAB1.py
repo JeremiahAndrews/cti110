35521d63e1933821fe39c099548f06161be7103c
JeremiahAndrews <156948054+JeremiahAndrews@users.noreply.github.com>
2024-09-04 17:59:30 +0000

    changed code

---
+++ P1LAB1.py
@@ -0,0 +1,15 @@
+# Jeremiah Andrews 
+# 9/4/2024
+# P1LAB1
+# Input and Output
+-
+# the input function automatically saves the data as a string
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+print()
+print("Hello " + firstName + " " + lastName + "! " + "Welcome to CTI110")
+print()
+print(type(firstName))
+print()
+print(firstName,lastName)
+print()
